e04efff11072e4a0a614d73d744f4d916794c4c1
Matevž Jekovec <matevz@oasisprotocol.org>
2023-07-15 12:54:22 +0200

    Initial commit

---
+++ assets/404View.138753e1.js
@@ -0,0 +1,2 @@
+import{_ as e,o,c as s,e as t}from"./index.4b7ee666.js";const c={},n={class:"404"},_=t("h1",null,"Page not found",-1),a=[_];function r(d,i){return o(),s("div",n,a)}const l=e(c,[["render",r]]);export{l as default};
+//# sourceMappingURL=404View.138753e1.js.map
