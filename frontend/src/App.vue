d027f80b8939d746b79c4c3c3e2b02aa34cda5f8
lubej <9722540+lubej@users.noreply.github.com>
2023-07-17 12:28:01 +0200

    Adjust design for mobile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@ import AppHeader from '@/components/AppHeader.vue';
 <template>
   <AppHeader />
 
-  <main class="py-5 m-auto max-w-screen-md">
+  <main class="py-5 px-2 m-auto max-w-screen-md lg:px-0">
     <RouterView el="main" />
   </main>
 </template>
