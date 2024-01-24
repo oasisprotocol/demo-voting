e3e1f02f175fd5841b25c17d6e27e3c8eb295b2f
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-01-24 21:34:31 +0000

    update to sapphire-paratime 1.3.0 & latest sapphire-hardhat

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,4 +1,4 @@
-//import '@oasisprotocol/sapphire-hardhat';
+import '@oasisprotocol/sapphire-hardhat';
 import "@nomicfoundation/hardhat-ethers"
 import { promises as fs } from 'fs';
 import path from 'path';
